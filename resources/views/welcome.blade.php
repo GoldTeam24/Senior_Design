03626d7eea6e2c3ed2dc68751524514776704568
Kyle Cser <kcser@oplogic.com>
2016-12-15 21:04:53 -0500

    formatting

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,14 @@
+@include('header')
+
 @extends('app')
 @section('content')
-                <div class="title m-b-md">
-                    Prerequisite<br>Knowledge
-                </div>
 
-                <div class="links">
-                    <a href="{{ url('/search') }}">Search Concepts</a>
-                </div>
+    <div class="title m-b-md">
+        Prerequisite<br>Knowledge
+    </div>
+
+    <div class="links">
+        <a href="{{ url('/search') }}">Search Concepts</a>
+    </div>
 
 @stop
