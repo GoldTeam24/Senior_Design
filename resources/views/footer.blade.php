ce49127d69f234b7a03b5ed0b39f7fbc0252d0db
Kyle Cser <kcser@oplogic.com>
2016-12-15 21:16:24 -0500

    create footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,17 @@
+<head>
+    <style type="text/css">
+    	.footer {
+			position: absolute;
+			padding-top: 15px;
+			bottom: 0;
+			width: 100%;
+			height: 60px;
+			background-color: #f5f5f5;
+			border-top: 1px solid #e7e7e7;
+		}
+	</style>
+</head>
+
+<footer class="footer text-center">
+	&copy; Gold Team <?php echo date("Y"); ?>
+</footer>
