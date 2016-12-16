163457530648acf6c90f899f44a20949e1866a1c
Kyle Cser <kcser@oplogic.com>
2016-12-15 21:29:43 -0500

    removed copyright. add color to text.

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -8,10 +8,11 @@
 			height: 60px;
 			background-color: #f5f5f5;
 			border-top: 1px solid #e7e7e7;
+			color: #b76e79;
 		}
 	</style>
 </head>
 
 <footer class="footer text-center">
-	&copy; Gold Team <?php echo date("Y"); ?>
+	Gold Team <?php echo date("Y"); ?>
 </footer>
