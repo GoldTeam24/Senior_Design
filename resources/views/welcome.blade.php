7f1569d88cef80467dc392c0114d21081ad5b0e3
Kyle Cser <kcser@oplogic.com>
2017-01-30 19:05:26 -0500

    change style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,13 +2,24 @@
 @extends('app')
 @section('content')
 
-    <div class="title m-b-md">
-        Prerequisite<br>Knowledge
-    </div>
+	<style type="text/css">
+		div.jumbotron {
+			background-color: transparent;
+		}
+	</style>
 
-    <div class="links">
-        <a href="{{ url('/search') }}">Search Concepts</a>
-    </div>
+	<div class="jumbotron">
+		<h1 class="text-center"> Prerequisite Knowledge </h1>
+
+	    <div class="links text-center">
+	        <a href="{{ url('/search') }}">Search Concepts</a>
+	    </div>
+	</div>
+
+    {{-- <div class="title m-b-md">
+        Prerequisite<br>Knowledge
+    </div> --}}
+
 @stop
 
 @include('footer')
