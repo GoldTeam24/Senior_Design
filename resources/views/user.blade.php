fb3bd7f067ac345b989a2284ce0fee12009370a8
Kyle Cser <kcser@oplogic.com>
2017-02-27 21:20:36 -0500

    change some style, fix the button stacking

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -4,22 +4,28 @@
     <div class="row">
         <div class="col-xs-12">
             <h1 class="page-header">
-                Account Information
-                <div class="pull-right" role="group">
-                    <a class="btn btn-default" href="{{ route('user.edit', array('id' => Auth::user()->id))}}">
-                        Edit Account Information
-                    </a>
-                    {{ Form::open(['method' => 'DELETE', 'route' => ['user.destroy', Auth::user()->id], 'onsubmit' => 'return confirm("Are you sure you want to deactivate this account? All account information will be permanently lost.")']) }}
-                    {{ Form::submit('Deactivate Account', ['class' => 'btn btn-default']) }}
-                    {{ Form::close() }}
+                <div class="row">
+                    <div class="col-xs-8">
+                        Account Information
+                    </div>
+                    <div class="col-xs-4">
+                        <div class="pull-right" role="group">
+                            {{ Form::open(['method' => 'DELETE', 'route' => ['user.destroy', Auth::user()->id], 'onsubmit' => 'return confirm("Are you sure you want to deactivate this account? All account information will be permanently lost.")']) }}
+                                <a class="btn btn-default" href="{{ route('user.edit', array('id' => Auth::user()->id))}}">
+                                    Edit Account Information
+                                </a>
+                            {{ Form::submit('Deactivate Account', ['class' => 'btn btn-default']) }}
+                            {{ Form::close() }}
+                        </div>
+                    </div>
                 </div>
             </h1>
 
-            <h3><strong>Username</strong></h3>
-            <h4>{{ Auth::user()->name }}</h4>
+            <h3> Username </h3>
+            <p> {{ Auth::user()->name }} </p>
 
-            <h3><strong>Email</strong></h3>
-            <h4>{{ Auth::user()->email }}</h4>
+            <h3> Email </h3>
+            <p> {{ Auth::user()->email }} </p>
         </div>
     </div>
 
