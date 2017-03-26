fc96a765dfb4a002fa509ff96515dc56d44872b7
JustinHoyt <justinhoyt24@gmail.com>
2017-03-26 00:27:12 -0400

    added is_admin to database and the website now checks for it

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Route::get('search',[ 'as' => 'search', 'uses' =>  'SearchController@index' ]);
 
 // Concept Routes
-Route::get('concept/linkChild/create/{id}',[ 'as' => 'createChildLink', 'uses' => 'ConceptController@createChildLink']);
+Route::get('concept/linkChild/create/{id}',[ 'as' => 'createChildLink', 'uses' => 'ConceptController@createChildLink'])->middleware('admin');
 Route::get('concept/linkParent/create/{id}',[ 'as' => 'createParentLink', 'uses' => 'ConceptController@createParentLink']);
 Route::post('concept/linkChild/store',['as' => 'storeChildLink', 'uses' => 'ConceptController@storeChildLink']);
 Route::post('concept/linkParent/store',['as' => 'storeParentLink', 'uses' => 'ConceptController@storeParentLink']);
