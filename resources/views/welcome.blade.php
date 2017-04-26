0d568574698eea86642839a4b1a4106791b081b7
Kyle Cser <kcser@oplogic.com>
2017-04-26 15:54:40 -0400

    fix logout issue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="{{ asset('js/app.js') }}"></script>
 <script type="text/javascript">
     $(function(){
         $("#carousel").carousel();
